1ea525eabd7eae91ccca3f34177a534c3d7e677c
vinicsalles <vsmiranda4321@gmail.com>
2024-05-22 17:34:14 -0300

    update na Classe Usuario Entity

--- grupo1/src/main/java/org/serratec/trabalho/grupo1/model/Usuario.java
+++ grupo1/src/main/java/org/serratec/trabalho/grupo1/model/Usuario.java
@@ -3,6 +3,7 @@ package org.serratec.trabalho.grupo1.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.serratec.trabalho.grupo1.exception.MensagensValidator;
 
 import java.util.Date;
 import java.util.Objects;
@@ -14,70 +15,70 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "id_usuario")
-    @NotBlank(message="O usuário não pode ser vazio ou nullo")
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     private long id_usuario;
 
     @Column(name= "nome_usuario")
-    @Size(max = 40, message="Tamanho máximo 40")
-    @NotBlank(message="O nome não pode ser vazio ou nullo")
+    @Size(max = 40, message = MensagensValidator.INVALID_SIZE)
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     private String nome;
 
     @Column(name= "sobrenome_usuario")
-    @Size(max = 40, message="Tamanho máximo 40")
-    @NotBlank(message="O sobrenome não pode ser vazio ou nullo")
+    @Size(max = 40, message= MensagensValidator.INVALID_SIZE)
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     private String sobrenome;
 
     @Column(name= "email_usuario")
-    @Size(max = 40, message="Tamanho máximo 40")
-    @NotBlank(message="O email não pode ser vazio ou nullo")
+    @Size(max = 40, message= MensagensValidator.INVALID_SIZE)
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     private String email;
 
     @Column(name= "senha_usuario")
-    @Size(max = 40, message="Tamanho máximo 40")
-    @NotBlank(message="a senha não pode ser vazio ou nullo")
+    @Size(max = 40, message= MensagensValidator.INVALID_SIZE)
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     private String senha;
 
     @Column(name= "nascimento_usuario")
     private Date dataNascimento;
 
-
+    @NotBlank(message = MensagensValidator.NOT_BLANK)
     public long getId_usuario() {
         return id_usuario;
     }
 
-    public void setId_usuario(@NotBlank(message = "O usuário não pode ser vazio ou nullo") long id_usuario) {
+    public void setId_usuario(@NotBlank(message = MensagensValidator.NOT_BLANK) long id_usuario) {
         this.id_usuario = id_usuario;
     }
 
-    public @Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O nome não pode ser vazio ou nullo") String getNome() {
+    public @Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String getNome() {
         return nome;
     }
 
-    public void setNome(@Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O nome não pode ser vazio ou nullo") String nome) {
+    public void setNome(@Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String nome) {
         this.nome = nome;
     }
 
-    public @Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O sobrenome não pode ser vazio ou nullo") String getSobrenome() {
+    public @Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String getSobrenome() {
         return sobrenome;
     }
 
-    public void setSobrenome(@Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O sobrenome não pode ser vazio ou nullo") String sobrenome) {
+    public void setSobrenome(@Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String sobrenome) {
         this.sobrenome = sobrenome;
     }
 
-    public @Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O email não pode ser vazio ou nullo") String getEmail() {
+    public @Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String getEmail() {
         return email;
     }
 
-    public void setEmail(@Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "O email não pode ser vazio ou nullo") String email) {
+    public void setEmail(@Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String email) {
         this.email = email;
     }
 
-    public @Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "a senha não pode ser vazio ou nullo") String getSenha() {
+    public @Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String getSenha() {
         return senha;
     }
 
-    public void setSenha(@Size(max = 40, message = "Tamanho máximo 40") @NotBlank(message = "a senha não pode ser vazio ou nullo") String senha) {
+    public void setSenha(@Size(max = 40, message = MensagensValidator.INVALID_SIZE) @NotBlank(message = MensagensValidator.NOT_BLANK) String senha) {
         this.senha = senha;
     }
 
