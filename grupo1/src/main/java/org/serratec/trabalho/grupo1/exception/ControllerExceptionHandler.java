fbf766a3546ead2afb5f960ec7ddbd81c9c5863d
muryarth <arthur.mury@outlook.com>
2024-05-22 15:51:13 -0300

    fix: importando classe ErroResposta no ControllerHandlerException

--- grupo1/src/main/java/org/serratec/trabalho/grupo1/exception/ControllerExceptionHandler.java
+++ grupo1/src/main/java/org/serratec/trabalho/grupo1/exception/ControllerExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.serratec.trabalho.grupo1.exception.ErroResposta;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
