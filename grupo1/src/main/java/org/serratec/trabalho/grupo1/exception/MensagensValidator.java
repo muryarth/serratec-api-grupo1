8eaae8b7aca2a13a3af2188d21a00e7382263628
muryarth <arthur.mury@outlook.com>
2024-05-22 16:55:08 -0300

    feat: criando interface para padronização de mensagens de erro

---
+++ grupo1/src/main/java/org/serratec/trabalho/grupo1/exception/MensagensValidator.java
@@ -0,0 +1,7 @@
+package org.serratec.trabalho.grupo1.exception;
+
+public interface MensagensValidator {
+    public static String NOT_BLANK = "O campo não pode estar vazio ou nulo.";
+    public static String NOT_NULL = "O campo não pode ser nulo.";
+    public static String INVALID_SIZE = "Tamanho inválido.";
+}
