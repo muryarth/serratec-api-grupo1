0a88eee16cd096de8a0c9b2d86929b65abb36c8e
muryarth <arthur.mury@outlook.com>
2024-05-23 20:09:42 -0300

    feat: acrescentando nova mensagem de validação

--- grupo1/src/main/java/org/serratec/trabalho/grupo1/exception/MensagensValidator.java
+++ grupo1/src/main/java/org/serratec/trabalho/grupo1/exception/MensagensValidator.java
@@ -1,7 +1,9 @@
 package org.serratec.trabalho.grupo1.exception;
 
 public interface MensagensValidator {
-    public static String NOT_BLANK = "O campo não pode estar vazio ou nulo.";
-    public static String NOT_NULL = "O campo não pode ser nulo.";
+    public static String NOT_FOUND = "Nenhum item foi encontrado para o valor fornecido.";
+    public static String NOT_BLANK = "O campo não pode ser vazio ou nulo.";
+    public static String NOT_NULL = "O campo não pode ser vazio ou nulo.";
     public static String INVALID_SIZE = "Tamanho inválido.";
+    public static String INVALID_MAX_SIZE = "O valor inserido viola o tamanho máximo estabelecido.";
 }
