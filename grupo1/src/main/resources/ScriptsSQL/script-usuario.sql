5594e9898183ba874ee7a57b1eac1bedf4675ad5
muryarth <arthur.mury@outlook.com>
2024-05-26 15:03:15 -0300

    fix: corrigindo SQL de usuario

--- grupo1/src/main/resources/ScriptsSQL/script-usuario.sql
+++ grupo1/src/main/resources/ScriptsSQL/script-usuario.sql
@@ -1,92 +1,92 @@
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1995-05-12', 'usuario1@email.com', 'João', 'senha123', 'Silva');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1988-09-30', 'usuario2@email.com', 'Maria', 'abc123', 'Santos');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1979-03-25', 'usuario3@email.com', 'Pedro', 'qwerty', 'Oliveira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2001-11-10', 'usuario4@email.com', 'Ana', 'senha456', 'Rocha');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1993-08-15', 'usuario5@email.com', 'Carlos', '123456', 'Pereira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1985-02-04', 'usuario6@email.com', 'Juliana', 'senha789', 'Ferreira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1977-10-20', 'usuario7@email.com', 'Fernando', 'senhaabc', 'Almeida');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1990-06-28', 'usuario8@email.com', 'Mariana', 'senhaXYZ', 'Cunha');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2003-04-03', 'usuario9@email.com', 'Ricardo', '123abc', 'Martins');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1980-12-09', 'usuario10@email.com', 'Camila', 'abcXYZ', 'Lima');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1998-07-18', 'usuario11@email.com', 'Gustavo', 'XYZ123', 'Barbosa');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1992-01-31', 'usuario12@email.com', 'Aline', 'senhaABC', 'Mendes');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1983-05-14', 'usuario13@email.com', 'Lucas', 'XYZabc', 'Rodrigues');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2000-09-06', 'usuario14@email.com', 'Tatiane', '789XYZ', 'Farias');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1987-03-22', 'usuario15@email.com', 'Roberto', 'abc789', 'Nunes');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1996-11-27', 'usuario16@email.com', 'Patrícia', 'senha123ABC', 'Gomes');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1978-08-05', 'usuario17@email.com', 'Fábio', '789abcXYZ', 'Araújo');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1989-02-17', 'usuario18@email.com', 'Carolina', 'senhaXYZ789', 'Vieira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2002-06-11', 'usuario19@email.com', 'Eduardo', 'XYZ789abc', 'Melo');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1976-12-23', 'usuario20@email.com', 'Renata', '123XYZabc', 'Dias');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1991-10-08', 'usuario21@email.com', 'Gabriel', 'abc123XYZ', 'Oliveira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1984-04-30', 'usuario22@email.com', 'Sandra', '789XYZabc', 'Souza');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1999-12-13', 'usuario23@email.com', 'Marcelo', 'XYZabc123', 'Silveira');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2004-07-09', 'usuario24@email.com', 'Amanda', 'senha123XYZ', 'Santana');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1974-01-27', 'usuario25@email.com', 'Robson', 'abcXYZ123', 'Gonçalves');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1997-09-14', 'usuario26@email.com', 'Vanessa', 'XYZ123abc', 'Costa');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2006-03-04', 'usuario27@email.com', 'Leandro', '789abcXYZ', 'Lima');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1975-11-18', 'usuario28@email.com', 'Mônica', 'XYZ789abc', 'Mendes');
 
-INSERT INTO relacao (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1990-05-02', 'usuario29@email.com', 'Luiz', '123abcXYZ', 'Oliveira');
 
-INSERT INTO usuario (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('1996-06-02', 'usuario30@email.com', 'Afonso', '133abcXYZ', 'Santos');
 
-INSERT INTO usuario (nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
+INSERT INTO usuario (data_nascimento_usuario, email_usuario, nome_usuario, senha_usuario, sobrenome_usuario)
 VALUES ('2000-03-01', 'usuario31@email.com', 'Gustavo', '123abasdf', 'Silva');
